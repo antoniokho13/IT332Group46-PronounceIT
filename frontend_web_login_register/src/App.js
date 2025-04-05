b26d68495e92d19b2183b2de26d99a92426a1508
rommelmars <rommelpobadora@gmail.com>
2025-04-05 11:25:30 +0800

    MERGED LOGIN AND HOME PAGE
    MERGED LOGIN AND HOME PAGE

--- frontend_web_login_register/src/App.js
+++ frontend_web_login_register/src/App.js
@@ -1,11 +1,19 @@
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/home';
 import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
